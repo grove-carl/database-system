3f4aaa94e99be5a7951906aaa2964ca11898a5ca
Xiangrui Dong <samdongxiangrui@outlook.com>
2022-04-17 19:35:59 +0800

    add test case for the scenario that provide multiple column definitions when create table

--- src/test/java/QueryExecutionEngineTest.java
+++ src/test/java/QueryExecutionEngineTest.java
@@ -45,6 +45,36 @@ class QueryExecutionEngineTest {
     }
 
     @Test
+    void should_create_table_when_execute_create_table_statement_given_multiple_columns_are_defined() {
+        String tableName = "user";
+        String idColumnName = "id";
+        String idColumnType = "Integer";
+        String usernameColumnName = "username";
+        String usernameColumnType = "String";
+        String passwordColumnName = "password";
+        String passwordColumnType = "String";
+        List<ColumnDefinition> columnDefinitions = List.of(
+                ColumnDefinition.builder().columnName(idColumnName).columnType(idColumnType).build(),
+                ColumnDefinition.builder().columnName(usernameColumnName).columnType(usernameColumnType).build(),
+                ColumnDefinition.builder().columnName(passwordColumnName).columnType(passwordColumnType).build()
+        );
+
+        queryExecutionEngine.execute(tableName, columnDefinitions);
+
+        Table userTable = database.getTable("user");
+        assertNotNull(userTable);
+
+        List<ColumnDefinition> actualColumnDefinitions = userTable.getDefinitions();
+        assertEquals(3, actualColumnDefinitions.size());
+        assertEquals(idColumnName, actualColumnDefinitions.get(0).getColumnName());
+        assertEquals(idColumnType, actualColumnDefinitions.get(0).getColumnType());
+        assertEquals(usernameColumnName, actualColumnDefinitions.get(1).getColumnName());
+        assertEquals(usernameColumnType, actualColumnDefinitions.get(1).getColumnType());
+        assertEquals(passwordColumnName, actualColumnDefinitions.get(2).getColumnName());
+        assertEquals(passwordColumnType, actualColumnDefinitions.get(2).getColumnType());
+    }
+
+    @Test
     void should_drop_table_when_execute_drop_table_statement_given_one_table_exist() {
         String tableName = "user";
         String columnName = "id";
